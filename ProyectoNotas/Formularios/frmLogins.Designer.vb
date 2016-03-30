8d559a40d2ae34016479f2708fc7795ffe71bc40
Framym Maradiaga <framym@hotmail.com>
2016-03-30 09:47:12 -0600

    Cambios en el formulario logins
    Se cambio el diseño del formulario logins, para que se viera minimalista

--- ProyectoNotas/Formularios/frmLogins.Designer.vb
+++ ProyectoNotas/Formularios/frmLogins.Designer.vb
@@ -22,6 +22,8 @@ Partial Class frmLogins
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogins))
         Me.PictureAlumno = New DevExpress.XtraEditors.PictureEdit()
         Me.PictureEmpleado = New DevExpress.XtraEditors.PictureEdit()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         CType(Me.PictureAlumno.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureEmpleado.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -29,8 +31,10 @@ Partial Class frmLogins
         'PictureAlumno
         '
         Me.PictureAlumno.EditValue = CType(resources.GetObject("PictureAlumno.EditValue"), Object)
-        Me.PictureAlumno.Location = New System.Drawing.Point(247, 32)
+        Me.PictureAlumno.Location = New System.Drawing.Point(247, 69)
         Me.PictureAlumno.Name = "PictureAlumno"
+        Me.PictureAlumno.Properties.Appearance.BackColor = System.Drawing.Color.LightGray
+        Me.PictureAlumno.Properties.Appearance.Options.UseBackColor = True
         Me.PictureAlumno.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
         Me.PictureAlumno.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.[Auto]
         Me.PictureAlumno.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch
@@ -40,22 +44,54 @@ Partial Class frmLogins
         'PictureEmpleado
         '
         Me.PictureEmpleado.EditValue = CType(resources.GetObject("PictureEmpleado.EditValue"), Object)
-        Me.PictureEmpleado.Location = New System.Drawing.Point(23, 32)
+        Me.PictureEmpleado.Location = New System.Drawing.Point(23, 69)
         Me.PictureEmpleado.Name = "PictureEmpleado"
+        Me.PictureEmpleado.Properties.Appearance.BackColor = System.Drawing.Color.LightGray
+        Me.PictureEmpleado.Properties.Appearance.Options.UseBackColor = True
         Me.PictureEmpleado.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
         Me.PictureEmpleado.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.[Auto]
         Me.PictureEmpleado.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch
         Me.PictureEmpleado.Size = New System.Drawing.Size(197, 179)
         Me.PictureEmpleado.TabIndex = 2
         '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 20.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(172, Byte), Integer), CType(CType(228, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(-1, -1)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(428, 39)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Eliga su Tipo de Usuario"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.DisabledLinkColor = System.Drawing.Color.Transparent
+        Me.LinkLabel1.Font = New System.Drawing.Font("Tahoma", 20.0!)
+        Me.LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(172, Byte), Integer), CType(CType(228, Byte), Integer))
+        Me.LinkLabel1.Location = New System.Drawing.Point(424, -1)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(42, 39)
+        Me.LinkLabel1.TabIndex = 4
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "X"
+        Me.LinkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'frmLogins
         '
+        Me.Appearance.BackColor = System.Drawing.Color.LightGray
+        Me.Appearance.Options.UseBackColor = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(465, 240)
+        Me.ClientSize = New System.Drawing.Size(465, 276)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureEmpleado)
         Me.Controls.Add(Me.PictureAlumno)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.HtmlText = "Eliga su tipo de usuario"
         Me.Name = "frmLogins"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -67,4 +103,6 @@ Partial Class frmLogins
 
     Friend WithEvents PictureAlumno As DevExpress.XtraEditors.PictureEdit
     Friend WithEvents PictureEmpleado As DevExpress.XtraEditors.PictureEdit
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label1 As Label
 End Class
--- ProyectoNotas/Formularios/frmLogins.vb
+++ ProyectoNotas/Formularios/frmLogins.vb
@@ -12,4 +12,18 @@
             LoginEmpleado.Show()
         End With
     End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Close()
+    End Sub
+
+    Private Sub LinkLabel1_MouseHover(sender As Object, e As EventArgs) Handles LinkLabel1.MouseHover
+        LinkLabel1.BackColor = Color.Red
+        LinkLabel1.LinkColor = Color.LightGray
+    End Sub
+
+    Private Sub LinkLabel1_MouseLeave(sender As Object, e As EventArgs) Handles LinkLabel1.MouseLeave
+        LinkLabel1.BackColor = Color.Transparent
+        LinkLabel1.LinkColor = Color.DodgerBlue
+    End Sub
 End Class
