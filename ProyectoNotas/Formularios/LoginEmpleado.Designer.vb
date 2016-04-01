bec45d5e9d1d71fd9137e47ca431a74fdbc4e04a
Framym Maradiaga <framym@hotmail.com>
2016-04-01 00:00:48 -0600

    Se cambio el diseño de LoginEmpleado

--- ProyectoNotas/Formularios/LoginEmpleado.Designer.vb
+++ ProyectoNotas/Formularios/LoginEmpleado.Designer.vb
@@ -17,18 +17,143 @@ Partial Class LoginEmpleado
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LoginEmpleado))
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.txtContrasenia = New System.Windows.Forms.TextBox()
+        Me.PictureEdit1 = New DevExpress.XtraEditors.PictureEdit()
+        CType(Me.PictureEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.BackColor = System.Drawing.Color.DimGray
+        Me.btnIngresar.FlatAppearance.BorderSize = 0
+        Me.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnIngresar.Font = New System.Drawing.Font("Tahoma", 12.0!)
+        Me.btnIngresar.ForeColor = System.Drawing.Color.LightGray
+        Me.btnIngresar.Location = New System.Drawing.Point(111, 330)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(86, 33)
+        Me.btnIngresar.TabIndex = 0
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.DimGray
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 16.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.LightGray
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(319, 44)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "UE - Ingrese su Usuario"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 12.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.DimGray
+        Me.Label2.Location = New System.Drawing.Point(52, 187)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 19)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Usuario"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.DimGray
+        Me.LinkLabel1.Location = New System.Drawing.Point(110, 366)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(89, 13)
+        Me.LinkLabel1.TabIndex = 4
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Cancelar Ingreso"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 12.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.DimGray
+        Me.Label3.Location = New System.Drawing.Point(52, 257)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 19)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Contraseña"
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Font = New System.Drawing.Font("Tahoma", 12.0!)
+        Me.txtUsuario.Location = New System.Drawing.Point(55, 209)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(218, 27)
+        Me.txtUsuario.TabIndex = 6
+        '
+        'txtContrasenia
+        '
+        Me.txtContrasenia.Font = New System.Drawing.Font("Tahoma", 12.0!)
+        Me.txtContrasenia.Location = New System.Drawing.Point(55, 279)
+        Me.txtContrasenia.Name = "txtContrasenia"
+        Me.txtContrasenia.Size = New System.Drawing.Size(218, 27)
+        Me.txtContrasenia.TabIndex = 7
+        '
+        'PictureEdit1
+        '
+        Me.PictureEdit1.EditValue = CType(resources.GetObject("PictureEdit1.EditValue"), Object)
+        Me.PictureEdit1.Location = New System.Drawing.Point(91, 54)
+        Me.PictureEdit1.Name = "PictureEdit1"
+        Me.PictureEdit1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent
+        Me.PictureEdit1.Properties.Appearance.Options.UseBackColor = True
+        Me.PictureEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
+        Me.PictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.[Auto]
+        Me.PictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch
+        Me.PictureEdit1.Properties.ZoomPercent = 150.0R
+        Me.PictureEdit1.Size = New System.Drawing.Size(143, 130)
+        Me.PictureEdit1.TabIndex = 8
         '
         'LoginEmpleado
         '
+        Me.Appearance.BackColor = System.Drawing.Color.DarkGray
+        Me.Appearance.Options.UseBackColor = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(334, 407)
+        Me.ClientSize = New System.Drawing.Size(317, 399)
+        Me.Controls.Add(Me.PictureEdit1)
+        Me.Controls.Add(Me.txtContrasenia)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnIngresar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "LoginEmpleado"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "LoginEmpleado"
+        CType(Me.PictureEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtUsuario As TextBox
+    Friend WithEvents txtContrasenia As TextBox
+    Friend WithEvents PictureEdit1 As DevExpress.XtraEditors.PictureEdit
 End Class
--- ProyectoNotas/Formularios/LoginEmpleado.vb
+++ ProyectoNotas/Formularios/LoginEmpleado.vb
@@ -2,4 +2,9 @@
     Private Sub LoginEmpleado_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         frmLogins.Show()
     End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Close()
+        frmLogins.Show()
+    End Sub
 End Class
